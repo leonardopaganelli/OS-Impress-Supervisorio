65944ede6d720fa18a6abcaef5d51be989313513
Leonardo <leonardo.paganelli@iteris.com.br>
2018-05-06 21:01:55 -0300

    refatoracao da controller e view de monitoramento

--- app/components/monitoramento/monitoramento.controller.js
+++ app/components/monitoramento/monitoramento.controller.js
@@ -8,61 +8,66 @@
     MonitoramentoController.$inject = [
         '$scope',
         '$interval',
+        'monitoramentoService',
         'APP_CONFIG'
     ];
 
-    function MonitoramentoController($scope, $interval, APP_CONFIG) {
+    function MonitoramentoController($scope, $interval, monitoramentoService, APP_CONFIG) {
+
+        $scope.vm = { }
+        $scope.fn = {
+            lerArquivo: LerArquivo,
+            trataRetornoArquivo: TrataRetornoArquivo
+        }
 
         Init();
 
-        function Init() { };
+        function Init() {
+            for (var i in $scope.computadores) {
+                $scope.fn.lerArquivo(i);
+            }
+        };
 
         $scope.computadores = APP_CONFIG.ATIVOS;
-
-        for (var i in $scope.computadores) {
-            lerArquivo(i);
-        }
 
         var segundos = 0.1;
         var delay = segundos * 1000;
         $interval(function(){
             for (var i in $scope.computadores) {
-                lerArquivo(i);
+                $scope.fn.lerArquivo(i);
             }
-
         },delay);
 
-        function lerArquivo(index){
+        function LerArquivo(index){
 
-            var xhr = new XMLHttpRequest(),
-                method = "GET",
-                url = $scope.computadores[index].arquivo;
-            xhr.open(method, url, true);
-
-            xhr.onreadystatechange = function(){
-                if (this.readyState == 4 && this.status == 200) {
-                    if (index > -1)
-                        // $scope.computadores[index].status = xhr.response;
-                        if(xhr.response == 0){
-                            $scope.computadores[index].status = "Ativo";
-                            $scope.computadores[index].estilo.frente = "tile_true";
-                            $scope.computadores[index].estilo.borda = "tile_border_true";
-                            $scope.computadores[index].estilo.mapa = "map_true";
-                            $scope.computadores[index].imagem.icone = "./assets/img/ativo.png";
-                        }else if(xhr.response == 1){
-                            $scope.computadores[index].estilo.frente = "tile_false";
-                            $scope.computadores[index].status = "Inativo";
-                            $scope.computadores[index].estilo.borda = "tile_border_false";
-                            $scope.computadores[index].estilo.mapa = "map_false";
-                            $scope.computadores[index].imagem.icone = "./assets/img/inativo.png";
-                        } else {
-                            alert("erro");
-                        }
-                }
+            var filtroData = {
+                url: $scope.computadores[index].arquivo
             };
-            xhr.send();
+
+            monitoramentoService
+                .getConteudoArquivo(filtroData)
+                .then(function(response){
+                    if (response.status === 200) {
+                        $scope.computadores[index].ativo = $scope.fn.trataRetornoArquivo(response.data);
+                    } else {
+                        console.log(response.status, response.statusText);
+                    }
+                }, function (responseError) {
+                    console.log(responseError.status, responseError.statusText);
+                });
+
         };
 
+        function TrataRetornoArquivo(status) {
+            switch(status){
+                case "0":
+                    return true;
+                case "1":
+                    return false;
+                default:
+                    console.log('Status nao reconhecido')
+                    return false;
+            }
+        }
     }
-
 })();
